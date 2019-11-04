a6c8926c8090a14eeb8f4e283db38fa384dcf928
ms <in1ck@icloud.com>
2019-11-04 17:24:40 +0100

    completed constructor

--- src/petrinet/PetriNet.java
+++ src/petrinet/PetriNet.java
@@ -1,8 +1,9 @@
 package petrinet;
 
-
-import java.util.*;
-import java.util.concurrent.Semaphore;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class PetriNet<T> {
     private final Tokens tokens;
@@ -10,17 +11,10 @@ public class PetriNet<T> {
 
     public PetriNet(Map<T, Integer> initial, boolean fair) {
         HashMap<Place, Integer> map = new HashMap<>();
-        for (T i: initial.keySet()) {
+        for (T i: initial.keySet())
             map.put(Place.make(i), initial.get(i));
-        }
         tokens = new Tokens(map);
-        if (fair) {
-            strategy = new FairCondition<T>(tokens);
-        } else {
-            strategy = new UnfairCondition<T>(tokens);
-        }
-
-        assert(false); // todo
+        strategy = makeCondition(tokens, fair);
     }
 
     public Set<Map<T, Integer>> reachable(Collection<Transition<T>> transitions) {
@@ -30,7 +24,7 @@ public class PetriNet<T> {
     public Transition<T> fire(Collection<Transition<T>> transitions) throws InterruptedException {
         Map<Place, Integer> output;
         if (transitions.isEmpty()) {
-            throw new IllegalArgumentException("empty set argument");
+            throw new IllegalArgumentException("empty set");
         }
         Transition<T> ans = strategy.resolve(transitions);
         tokens.payFor(ans.getInput());
@@ -47,5 +41,12 @@ public class PetriNet<T> {
 
     private void addOutput(T key, Transition<T> transition) {
         Place.make(key).addOutput(transition.general());
+    }
+
+    private TokenCondition<T> makeCondition(Tokens tokens, boolean fair) {
+        if (fair)
+            return new FairCondition<>(tokens);
+        else
+            return new UnfairCondition<>(tokens);
     }
 }

6174d86a72e120237389c3bf1d082dc191d1b275
ms <in1ck@icloud.com>
2019-11-14 11:14:24 +0100

    enabled inhibitor and reset edges

--- src/petrinet/Transition.java
+++ src/petrinet/Transition.java
@@ -1,39 +1,8 @@
 package petrinet;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Transition<T> {
-    private static class Data {
-        final Map<Place, Integer> input, output;
-        final Set<Place> inhibitor, reset;
-
-        Data(
-                Map<Place,
-                Integer> input,
-                Collection<Place> reset,
-                Collection<Place> inhibitor,
-                Map<Place, Integer> output
-        ) {
-            this.input = makeView(input);
-            this.inhibitor = makeView(inhibitor);
-            this.reset = makeView(reset);
-            this.output = makeView(output);
-        }
-
-        private Map<Place, Integer> makeView(Map<Place, Integer> map) {
-            return Collections.unmodifiableMap(Map.copyOf(map));
-        }
-
-        private Set<Place> makeView(Collection<Place> collection) {
-            return Collections.unmodifiableSet(Set.copyOf(collection));
-        }
-    }
-
     private static class MapWriter<K, V> {
         private final Map<K, V> map;
 
@@ -62,48 +31,60 @@ public class Transition<T> {
             return ans.toString();
         }
     }
-    private final Data data;
+
+    final Map<Place, Integer> input, output;
+    final Set<Place> inhibitor, reset;
 
     public Transition(Map<T, Integer> input, Collection<T> reset, Collection<T> inhibitor, Map<T, Integer> output) {
-        assert(reset.isEmpty()); // todo: reset is not implemented
-        assert(inhibitor.isEmpty()); // todo: inhibitor is not implemented
-        this.data = new Data(asPlaces(input), asPlaces(reset), asPlaces(inhibitor), asPlaces(output));
+        this.input = asPlaces(input);
+        this.inhibitor = asPlaces(inhibitor);
+        this.reset = asPlaces(reset);
+        this.output = asPlaces(output);
     }
 
-    public Map<Place, Integer> getInput() {
-        return data.input;
+    boolean isUnblocked(Marking marking) {
+        for (Place i: inhibitor) {
+            if (marking.getCount(i) > 0)
+                return false;
+        }
+        return true;
     }
 
-    public Map<Place, Integer> getOutput() {
-        return data.output;
+    public boolean isInputReady(Marking marking) {
+        for (Place i: input.keySet()) {
+            if (marking.getCount(i) < input.get(i))
+                return false;
+        }
+        return true;
     }
 
-    private void writeMap(Map m, StringBuilder s) {
-
+    ImmutableMarking step(Marking start) {
+        MutableMarking marking = new MutableMarking(start);
+        marking.payFor(input);
+        marking.reset(reset);
+        marking.addTokens(output);
+        return new ImmutableMarking(marking);
     }
 
     private Map<Place, Integer> asPlaces(Map<T, Integer> map) {
         HashMap<Place, Integer> ans = new HashMap<>();
         for (T i: map.keySet())
             ans.put(Place.make(i), map.get(i));
-        return ans;
+        return Collections.unmodifiableMap(Map.copyOf(ans));
     }
 
     private Set<Place> asPlaces(Collection<T> collection) {
         HashSet<Place> ans = new HashSet<>();
         for (T i: collection)
             ans.add(Place.make(i));
-        return ans;
+        return Collections.unmodifiableSet(Set.copyOf(ans));
     }
 
     @Override
     public String toString() {
         return "Transition" +
-                "\ninput: " + new MapWriter<>(data.input) +
-                "\noutput: " + new MapWriter<>(data.output);
+                "\ninput: " + new MapWriter<>(input) +
+                "\noutput: " + new MapWriter<>(output);
     }
 
-    private Transition(Data data) {
-        this.data = data;
-    }
 }
